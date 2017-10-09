a8a36be7cf8d9b514a2939b4ccc2db9afa8f42ee
SnorriArinbjarnar <snorriarinbjarnar@gmail.com>
2017-10-09 10:49:43 +0100

    test for empty string

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,6 +2,18 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
+	public int add(String[] text)
+	{
+		if(text.equals(""))
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	
 	
 	public static void main(String[] args)
 	{
